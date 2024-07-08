ef6e47d5d1eda4fb8c3bcb5034beddbcf1865d5a
Shuflduf <shuflduf@gmail.com>
2024-07-07 19:37:18 -0600

    new options for gun

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -6,12 +6,28 @@ extends Node2D
 @export var clip = 6
 @export var reserve = 20
 
+@export var automatic = false
+
 @onready var barrel: Node2D = $barrel
 @onready var spawner: SpawnerComponent = $barrel/SpawnerComponent
+
+var shoot_cooldown = fire_speed
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		look_at(get_global_mouse_position())
 		
 	if event.is_action_pressed("mouse_left"):
+		shoot()
+
+func shoot():
+	if shoot_cooldown <= 0:
 		add_child(spawner.spawn(bullet))
+		shoot_cooldown = fire_speed
+
+func _process(delta: float) -> void:
+	if shoot_cooldown >= 0:
+		shoot_cooldown -= delta
+	
+	if automatic and Input.is_action_pressed("mouse_left"):
+		shoot()
