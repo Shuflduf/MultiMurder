41d7e9436c388f281a76b45ac16ee539e9ba2214
Shuflduf <shuflduf@gmail.com>
2024-07-09 14:04:15 -0600

    restructre feet
    to not copy paste to right later

--- scripts/avatar.gd
+++ scripts/avatar.gd
@@ -2,32 +2,39 @@ class_name Avatar
 extends Sprite2D
 
 @onready var left_foot: Sprite2D = $FootLeft
+@onready var foot_right: Sprite2D = $FootRight
+var feet = [left_foot, foot_right]
+
 @onready var face: Sprite2D = $Face
 
 var player: Player:
 	get:
 		return get_parent()
 
-#var move_dir: Vector2
-#var moving_foot = false
-#
-#var left_foot_position: Vector2
+var move_dir: Vector2
+var moving_foot = [false, false]
+
+var foot_pin = [Vector2.ZERO, Vector2.ZERO]
 
 var look_dir: Vector2
 
 func _process(_delta: float) -> void:	
 	face.position = look_dir / 200
-	#if !move_dir.is_zero_approx() and !moving_foot: 
-		#left_foot.position = -player.global_position / 4
-		##left_foot.position = -global_position / 4
-		#if left_foot.position.length() > 20:
+	if !move_dir.is_zero_approx(): 
+		for index in feet.size:
+			feet[index].global_position = foot_pin[index]
+		left_foot.global_position = left_foot_position
+		#left_foot.position = -global_position / 4
+		if left_foot.position.length() > 20:
 			#var new_dir = (left_foot.position - player.global_position)
 			#print(new_dir)
-			##left_foot_position = global_position + (move_dir * 150)
-			##move_foot(left_foot_position)
-			##
-	#else:
-		#left_foot.position = Vector2.ZERO
+			left_foot_position = global_position + (move_dir * 40)
+			#left_foot_position = global_position + (move_dir * 150)
+			#move_foot(left_foot_position)
+			#
+	else:
+		left_foot.position = Vector2.ZERO
+		left_foot_position = global_position
 
 #func move_foot(new_pos: Vector2):
 	#moving_foot = true
