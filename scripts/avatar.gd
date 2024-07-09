6c8c3a134907c127782486670250151dc6356caf
Shuflduf <shuflduf@gmail.com>
2024-07-09 13:09:35 -0600

    foot moves smoothly

--- scripts/avatar.gd
+++ scripts/avatar.gd
@@ -4,15 +4,26 @@ extends Sprite2D
 @onready var left_foot: Sprite2D = $FootLeft
 
 var move_dir: Vector2
+var moving_foot = false
 
 var left_foot_position: Vector2
 
 func _process(_delta: float) -> void:
-	if !move_dir.is_zero_approx(): 
-		left_foot.global_position = left_foot_position
+	print(left_foot_position)
+	left_foot.global_position = left_foot_position
+	if !move_dir.is_zero_approx() and !moving_foot: 
 		if (left_foot.global_position - global_position).length() > 100:
-			left_foot_position = global_position + (move_dir * 50)
+			move_foot()
 			
-	else:
-		left_foot.position = Vector2.ZERO
+	#else:
+		#left_foot.position = Vector2.ZERO
 
+func move_foot():
+	moving_foot = true
+	var foot_tween = get_tree().create_tween()
+	foot_tween.tween_property(self, "left_foot_position", \
+			global_position + (move_dir * 50), 0.4)\
+			.set_ease(Tween.EASE_IN_OUT)\
+			.set_trans(Tween.TRANS_CIRC)
+	await foot_tween.finished
+	moving_foot = false
