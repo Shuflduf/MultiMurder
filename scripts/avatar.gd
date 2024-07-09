9919dd09731c22223306a9c28e79c9bbbeb76d46
Shuflduf <shuflduf@gmail.com>
2024-07-09 14:17:11 -0600

    feet move with velocity

--- scripts/avatar.gd
+++ scripts/avatar.gd
@@ -20,16 +20,13 @@ var look_dir: Vector2
 func _process(_delta: float) -> void:	
 	face.position = look_dir / 200
 	
-
-	if !move_dir.is_zero_approx(): 
-		for index in feet.size():
-			print(feet[index])
-			feet[index].global_position = foot_pin[index]
-			if feet[index].position.length() > 20:
-				foot_pin[index] = global_position + (move_dir * 40)
-
-	else:
-		for i in feet.size():
+	for i in feet.size():
+		if !move_dir.is_zero_approx(): 
+			print(feet[i])
+			feet[i].global_position = foot_pin[i]
+			if feet[i].position.length() > 20:
+				foot_pin[i] = global_position + (move_dir * 40)
+		else:
 			feet[i].position = Vector2.ZERO
 			foot_pin[i] = global_position
 
